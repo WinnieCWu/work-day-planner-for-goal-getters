8bfb7f41fe0d3ad1ee1325ca522b70285eae2ded
WinnieCWu <winniechn7@gmail.com>
2022-02-12 11:27:24 -0800

    updated hourly planner and capturing date functionality

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-//display current day at top of calendar
-var currentDay = newDate();
-//have time blocks for standard business hours
+//display current day and time at top of calendar
+//current time variable
+var currentTime = moment().hours();
+console.log(currentTime);
+
+//hours variable for tasks
+var hours = $(".hour");
+console.log(hours);
+
+console.log(hours[0].dataset.id);
+
+//target save button
+var saveButton = $(".saveBtn")
+console.log(saveButton);
+
+//target textarea
+var taskDescriptionInput = $("textarea".val());
+
+var currentDay = $("#currentDay");
+var currentDayMoment = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentDay);
+currentDay.html(currentDayMoment);
+
+//when click the save button for that time block, the text for that event is saved in local storage
+saveButton.on("click", function(){
+
+})
 //each time block is color coded to indicate whether it is in the past (grey), present (red), or future (green)
 //when click into a time block, can enter an event
-//when click the save button for that time block, the text for that event is saved in local storage
-//when refresh, saved events persist
 
-$('#date').datepicker({
-    minDate: 0
-});
-$('#time').timepicker({
-    onHourShow: function( hour ) { 
-        // compare selected date with today
-        if ( $('#date').val() == $.datepicker.formatDate ( 'mm/dd/yy', currentDay) ) {
-            if ( hour <= now.getHours() ) {
-                return false;
-            }
-        }
-        return true;
-    }
-});​
